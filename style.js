280cde2c17ef2e2f3d69ce9f43bcec920fc4c930
alek <alek.schmierer2022@gmail.com>
2024-10-06 12:32:31 -0700

    Updated home page to Figma 2024 design.

--- style.js
+++ style.js
@@ -1,29 +1,3 @@
-// Get all the sidebar links
-var sidebarLinks = document.querySelectorAll('.sidebar a');
 
-document.querySelectorAll('.sidebar a').forEach(link => {
-    if (link.href === window.location.href) {
-      link.classList.add('current');
-    }
-  });
-
-
-// Loop through each link
-for (var i = 0; i < sidebarLinks.length; i++) {
-  sidebarLinks[i].addEventListener('mouseenter', function(event) {
-    event.target.classList.add('current1');
-  });
-  sidebarLinks[i].addEventListener('mouseleave', function(event) {
-    event.target.classList.remove('current1');
-  });
-}
-
-function openModal(modalId) {
-  document.getElementById(modalId).style.display = "block";
-}
-
-function closeModal(modalId) {
-  document.getElementById(modalId).style.display = "none";
-}
 
 
