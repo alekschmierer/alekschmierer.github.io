aa40b2affc802a25195477d4bdc23e9581606ac3
alek <alek.schmierer2022@gmail.com>
2025-07-14 21:04:32 -0700

    Added YouTube Videos

---
+++ scripts/openModal.js
@@ -0,0 +1,8 @@
+//openModal.js
+function openModal(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+}
